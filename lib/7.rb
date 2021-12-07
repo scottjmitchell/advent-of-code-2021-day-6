e3b57eae8ac135ffcb9bf84f3fbfe98fca73dec7
Scott Mitchell <scott_j_mitchell@hotmail.co.uk>
2021-12-07 21:15:12 +0000

    day 7 part complete

--- lib/7.rb
+++ lib/7.rb
@@ -53,11 +53,61 @@ class GiantWhale
 
     min_fuel
   end
+
+  # Part 2
+
+  def get_fuel_amount_exponential(horizontal_positions, end_position)
+    fuel = 0
+
+    horizontal_positions.keys.each do |horizontal_position|
+      crab_fuel = 0
+      distance = 0
+
+      if horizontal_position.to_i > end_position
+        distance = (horizontal_position.to_i - end_position)
+      else
+        distance = (end_position - horizontal_position.to_i)
+      end
+
+      for step in 0..distance
+        crab_fuel += step
+      end
+
+
+      fuel += crab_fuel * horizontal_positions[horizontal_position]
+    end
+    fuel
+  end
+
+  def find_optimal_position_exponential(horizontal_positions)
+    crabs = get_all_crab_positions(horizontal_positions)
+    min = horizontal_positions
+            .collect { |crab| crab.to_i}
+            .min
+    max = horizontal_positions
+            .collect { |crab| crab.to_i}
+            .max
+
+    min_fuel = fuel_amount = get_fuel_amount_exponential(crabs, 0)
+    position = 0
+
+    for num in min..max
+      fuel_amount = get_fuel_amount_exponential(crabs, num)
+
+      if fuel_amount < min_fuel
+        min_fuel = fuel_amount
+        position = num
+      end
+    end
+
+    min_fuel
+  end
 end
 
 data = File.read("#{File.dirname(__FILE__)}/input.txt").split(",")
 gw = GiantWhale.new
 
-# print gw.get_all_crab_positions(data)
-puts gw.find_optimal_position(data)
+# puts gw.find_optimal_position(data)
+puts gw.find_optimal_position_exponential(data)
+
 
